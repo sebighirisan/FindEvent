b0e3425f736914b870aee36b596fe9084dbae544
sebighirisan <ghirisansebastian@yahoo.com>
2025-06-09 20:13:43 +0300

    edit backend

---
+++ FindEvent/src/main/java/com/FindEvent/FindEvent/FindEventApplication.java
@@ -0,0 +1,13 @@
+package com.FindEvent.FindEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindEventApplication.class, args);
+	}
+
+}
---
+++ FindEvent/src/test/java/com/FindEvent/FindEvent/FindEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FindEvent.FindEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
