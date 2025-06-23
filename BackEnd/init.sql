91889650db29706b9344cae248d41ab41cb96d1e
Daniel Lucaci <danilucaci48@gmail.com>
2025-06-23 14:07:20 +0300

    Added pgvector.

--- BackEnd/init.sql
+++ BackEnd/init.sql
@@ -3,3 +3,7 @@ CREATE ROLE "find-event-admin" PASSWORD 'find-event-pwd' LOGIN;
 
 -- create schemas
 ALTER SCHEMA "public" OWNER TO "find-event-admin";
+
+-- create extension
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS vector;
